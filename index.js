106e07b249fb2aafc93c5700a3fb0ab3087d1c7a
PhamVanSang2601 <96646248+PhamVanSang2601@users.noreply.github.com>
2022-02-20 11:00:05 +0700

    tong so duong

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let array = [];
+
 const submitEl = document.getElementById("submit").onclick = function (){
     const numberEl = +document.getElementById("number").value;
     array.push(numberEl);
     console.log(array);
 }
-const submitEl2 = document.getElementById("submit2").onlcik = function(){
-    const chooseEl = +document.getElementById("choose").value ;
+const submit2El = document.getElementById("submit2").onclick = function(){
+    
+    const chooseEl = +document.getElementById("choose").value;
+    if(chooseEl === 1){
+        let sum = 0;
+        for(let index = 0; index <= array.length; index++){
+            if(array[index] > 0 && array[index] % 1 === 0){
+                sum += array[index];
+            }
+        }
+        console.log(sum);
+    }
     
 }
