2342ef44cf0e34d4bba957f060043a646e56f37a
PhamVanSang2601 <96646248+PhamVanSang2601@users.noreply.github.com>
2022-02-21 23:57:13 +0700

    choose 3

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 let array = [];
-
+const resultEl = document.getElementById("result");
 const submitEl = document.getElementById("submit").onclick = function (){
     const numberEl = +document.getElementById("number").value;
-    array.push(numberEl);
+    if(numberEl % 1 === 0){
+        array.push(numberEl);
+    } else if (numberEl % 1 !== 0){
+        alert("NHẬP LẠI SỐ NGUYÊN");
+    }
     console.log(array);
 }
-const submit2El = document.getElementById("submit2").onclick = function(){
-    
+const submit2 = document.getElementById("submit2").onclick = function(){
     const chooseEl = +document.getElementById("choose").value;
     if(chooseEl === 1){
         let sum = 0;
         for(let index = 0; index <= array.length; index++){
-            if(array[index] > 0 && array[index] % 1 === 0){
+            if(array[index] > 0){
                 sum += array[index];
             }
         }
         console.log(sum);
+        resultEl.innerHTML +=`<p class="m-0">TỔNG SỐ DƯƠNG LÀ: ${sum}</p>`
+    } else if(chooseEl === 2){
+        let sum = 0;
+        for(let index = 0; index <= array.length; index++){
+            if(array[index] > 0){
+                sum += 1;
+            }
+        }
+        console.log(sum);
+        resultEl.innerHTML +=`<p class="m-0">CÓ: ${sum} SỐ DƯƠNG TRONG MẢNG</p>`
+        
+    } else if (chooseEl === 3) {
+        const minInArray = Math.min.apply(Math, array);
+        console.log(minInArray);
+        resultEl.innerHTML +=`<p class="m-0">SỐ NHỎ NHẤT TRONG MẢNG LÀ: ${minInArray}</p>`
+    } else if (chooseEl === 4){
+        
     }
-    
+
+    resultEl.style.display = "block";
 }
